ea75416d027a99be7d09ade0e0a24efa32c95b55
Артём <artem.v.rusanov@gmail.com>
2017-01-22 11:31:55 +0300

    tests

---
+++ CircularBuffer.Core.Tests/BufferServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using CircularBuffer.Core.Domain;
+using CircularBuffer.Core.Services;
+using NUnit.Framework;
+
+namespace CircularBuffer.Core.Tests
+{
+    [TestFixture]
+    public class BufferServiceTests
+    {
+        private IBufferService _sut;
+        private Buffer _buffer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _buffer = new Buffer(3);
+            _sut = new BufferService(_buffer);
+        }
+
+        [Test]
+        public void WriteTest()
+        {
+            _sut.Write(new Page { Content = "1"});
+
+            Assert.AreEqual("1", _buffer.Pages[0].Content);
+        }
+
+        [Test]
+        public void ReadTest()
+        {
+            var result = _sut.Read();
+
+            Assert.IsNull(result);
+        }
+    }
+}
