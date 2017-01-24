71e1b88f91b59e249096a39c1f4bb75b001fae43
Artem <artem.v.rusanov@gmail.com>
2017-01-24 09:38:52 +0300

    demo

--- CircularBuffer.Client.Console/Program.cs
+++ CircularBuffer.Client.Console/Program.cs
@@ -17,16 +17,20 @@ namespace CircularBuffer.Client.Console
             var buffer = new Buffer(3);
             _bufferService = new BufferService(buffer);
 
+            System.Console.WriteLine("Записываем..");
             _bufferService.Write(new Page { Content = "1" });
             buffer.ToConsole();
-           // System.Console.ReadLine();
+            System.Console.ReadLine();
 
+            System.Console.WriteLine("Читаем..");
+            var page = _bufferService.Read();
+            System.Console.WriteLine(page.Content);
+
+            System.Console.WriteLine("Записываем..");
             _bufferService.Write(new Page { Content = "2" });
             buffer.ToConsole();
-          //  System.Console.ReadLine();
-
-            buffer.ToConsole();
             System.Console.ReadLine();
+
             buffer.ToConsole();
             System.Console.ReadLine();
         }
