f2708a1602bf4649132549adc2661012f1107628
Artem <artem.v.rusanov@gmail.com>
2017-05-21 13:05:59 +0300

    все используемые методы добавил в интерфейс

--- AsteriskApiTest/AsteriskApiTest/IRingsService.cs
+++ AsteriskApiTest/AsteriskApiTest/IRingsService.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
 
 namespace AsteriskApiTest
 {
@@ -13,8 +10,47 @@ namespace AsteriskApiTest
     public interface IRingsService
     {
         /// <summary>
+        /// Завершенны звонки
+        /// </summary>
+        DataTable GetCompleteRings(DateTime start, DateTime end);
+
+        /// <summary>
+        /// Завершенны звонки
+        /// </summary>
+        DataTable GetCompleteRingsByKeyList(string keysList);
+
+        /// <summary>
+        /// Текущие звонки
+        /// </summary>
+        DataTable GetCurrentRings();
+
+        /// <summary>
+        /// Текущий звонок 
+        /// </summary>
+        /// <param name="num_from">Номер</param>
+        DataTable GetCurrentRing(string num_to);
+
+        /// <summary>
+        /// Текущий звонок если не сработал обычный алгоритм
+        /// по идее это такой звонок к-й был пропущен
+        /// </summary>
+        /// <param name="num_to"></param>
+        /// <returns></returns>
+        DataTable GetCurrentRingIfMissed(string num_to);
+
+        /// <summary>
         /// Данные для отчета по стоимости разговоров
         /// </summary>
         DataTable GetCallsForBillingReport(DateTime start, DateTime end);
+
+        DataTable ReportByOperators(DateTime start, DateTime end);
+
+        DataTable GetActivitiPeers(DateTime start, DateTime end);
+
+        DataTable GetOperatorsNotTacker(DateTime start, DateTime end);
+
+        DataTable GetCallCenterAnaliticalIndexes(DateTime start, DateTime end);
+
+        void SetQueueTimeout(long seconds);
     }
 }
