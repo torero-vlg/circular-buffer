f9781baeb5e2f93751a7e9a65cb7041b431411b9
Артём <artem.v.rusanov@gmail.com>
2017-05-21 13:13:40 +0300

    методы из интерфейса

--- AsteriskApiTest/AsteriskApiTest/JsonRingService.cs
+++ AsteriskApiTest/AsteriskApiTest/JsonRingService.cs
@@ -17,6 +17,50 @@ namespace AsteriskApiTest
         }
 
         /// <summary>
+        /// Завершенны звонки
+        /// </summary>
+        public DataTable GetCompleteRings(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Завершенны звонки
+        /// </summary>
+        public DataTable GetCompleteRingsByKeyList(string keysList)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Текущие звонки
+        /// </summary>
+        public DataTable GetCurrentRings()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Текущий звонок 
+        /// </summary>
+        /// <param name="num_from">Номер</param>
+        public DataTable GetCurrentRing(string num_to)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Текущий звонок если не сработал обычный алгоритм
+        /// по идее это такой звонок к-й был пропущен
+        /// </summary>
+        /// <param name="num_to"></param>
+        /// <returns></returns>
+        public DataTable GetCurrentRingIfMissed(string num_to)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// Данные для отчета по стоимости разговоров
         /// </summary>
         public DataTable GetCallsForBillingReport(DateTime start, DateTime end)
@@ -35,6 +79,31 @@ namespace AsteriskApiTest
 
             //преобразовать response.Result в DataTable
             return response.Result.ToDataTable();
+        }
+
+        public DataTable ReportByOperators(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetActivitiPeers(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetOperatorsNotTacker(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetCallCenterAnaliticalIndexes(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetQueueTimeout(long seconds)
+        {
+            throw new NotImplementedException();
         }
     }
 }
